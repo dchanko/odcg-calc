39c5091b1922c8da679c109f311216d1a8167259
David Chanko <david.chanko@gmail.com>
2017-03-09 20:58:35 -0500

    Validations.

--- src/services/calculator.js
+++ src/services/calculator.js
@@ -6,20 +6,88 @@ export function calculateScore(diamond, inclusion) {
   const inclusionValidation = validateInclusion(inclusion);
   if (!inclusionValidation.ok) return inclusionValidation;
 
-
 };
 
 let validateDiamondLength = (state) => {
-  return state.diamond.length ? [] : [{
+  return state.diamond.length ? {} : {
     diamond: {
       length: "Length required."
     }
-  }];
-}
+  };
+};
+
+let validateDiamondWidth = (state) => {
+  return state.diamond.width ? {} : {
+    diamond: {
+      width: "Width required."
+    }
+  };
+};
+
+let validateInclusionLength = (state) => {
+  return state.inclusion.length ? {} : {
+    inclusion: {
+      length: "Length required."
+    }
+  };
+};
+
+let validateInclusionWidth = (state) => {
+  return state.inclusion.width ? {} : {
+    inclusion: {
+      width: "Width required."
+    }
+  };
+};
+
+let validateInclusionContrastRequired = (state) => {
+  return state.inclusion.contrast ? {} : {
+    inclusion: {
+      contrast: "Contrast required."
+    }
+  };
+};
+
+let validateInclusionContrastOneToFive = (state) => {
+  const contrast = state.inclusion.contrast;
+  const valid = contrast >= 1 && contrast <= 5;
+  return valid ? {} : {
+    inclusion: {
+      contrast: "Must be between 1 and 5."
+    }
+  };
+};
+
+let validateInclusionPositionRequired = (state) => {
+  return state.inclusion.position ? {} : {
+    inclusion: {
+      position: "Position required."
+    }
+  };
+};
+
+let validateInclusionPositionOneToFour = (state) => {
+  const position = state.inclusion.position;
+  const valid = position >= 1 && position <= 4;
+  return valid ? {} : {
+    inclusion: {
+      position: "Must be between 1 and 4."
+    }
+  };
+};
 
 export const validations = {
   diamond: {
-    length: validateDiamondLength
+    length: validateDiamondLength,
+    width: validateDiamondWidth
+  },
+  inclusion: {
+    length: validateInclusionLength,
+    width: validateInclusionWidth,
+    contrast: validateInclusionContrastRequired,
+    contrastRange: validateInclusionContrastOneToFive,
+    position: validateInclusionPositionRequired,
+    positionRange: validateInclusionPositionOneToFour
   }
 };
 
--- src/services/calculator.test.js
+++ src/services/calculator.test.js
@@ -14,7 +14,8 @@ describe('calculator services', () => {
     let state = {};
     beforeEach(() => {
       state = {
-        diamond: {}
+        diamond: {},
+        inclusion: {}
       };
     });
 
@@ -22,12 +23,111 @@ describe('calculator services', () => {
 
       test('invalid', () => {
         state.diamond.length = null;
-        expect(validations.diamond.length(state).length).toBeGreaterThan(0);
+        expect(validations.diamond.length(state).diamond.length).toBeDefined();
       });
 
       test('valid', () => {
         state.diamond.length = 6.5;
-        expect(validations.diamond.length(state).length).toBe(0);
+        expect(validations.diamond.length(state)).toEqual({});
+      });
+
+    });
+
+    describe('diamond.width', () => {
+
+      test('invalid', () => {
+        state.diamond.width = null;
+        expect(validations.diamond.width(state).diamond.width).toBeDefined();
+      });
+
+      test('valid', () => {
+        state.diamond.width = 6.5;
+        expect(validations.diamond.width(state)).toEqual({});
+      });
+
+    });
+
+    describe('inclusion.length', () => {
+
+      test('invalid', () => {
+        state.inclusion.length = null;
+        expect(validations.inclusion.length(state).inclusion.length).toBeDefined();
+      });
+
+      test('valid', () => {
+        state.inclusion.length = 6.5;
+        expect(validations.inclusion.length(state)).toEqual({});
+      });
+
+    });
+
+    describe('inclusion.width', () => {
+
+      test('invalid', () => {
+        state.inclusion.width = null;
+        expect(validations.inclusion.width(state).inclusion.width).toBeDefined();
+      });
+
+      test('valid', () => {
+        state.inclusion.width = 6.5;
+        expect(validations.inclusion.width(state)).toEqual({});
+      });
+
+    });
+
+    describe('inclusion.contrast', () => {
+
+      test('invalid', () => {
+        state.inclusion.contrast = null;
+        expect(validations.inclusion.contrast(state).inclusion.contrast).toBeDefined();
+      });
+
+      test('valid', () => {
+        state.inclusion.contrast = 0.50;
+        expect(validations.inclusion.contrast(state)).toEqual({});
+      });
+
+    });
+
+     describe('inclusion.contrastRange', () => {
+
+      test('invalid', () => {
+        state.inclusion.contrast = 6;
+        expect(validations.inclusion.contrastRange(state).inclusion.contrast).toBeDefined();
+      });
+
+      test('valid', () => {
+        state.inclusion.contrast = 5;
+        expect(validations.inclusion.contrastRange(state)).toEqual({});
+      });
+
+    });
+
+
+    describe('inclusion.position', () => {
+
+      test('invalid', () => {
+        state.inclusion.position = null;
+        expect(validations.inclusion.position(state).inclusion.position).toBeDefined();
+      });
+
+      test('valid', () => {
+        state.inclusion.position = 4;
+        expect(validations.inclusion.position(state)).toEqual({});
+      });
+
+    });
+
+     describe('inclusion.positionRange', () => {
+
+      test('invalid', () => {
+        state.inclusion.position = 6;
+        expect(validations.inclusion.positionRange(state).inclusion.position).toBeDefined();
+      });
+
+      test('valid', () => {
+        state.inclusion.position = 4;
+        expect(validations.inclusion.positionRange(state)).toEqual({});
       });
 
     });
