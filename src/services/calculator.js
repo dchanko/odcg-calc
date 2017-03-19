86d1bf16932d8c372d66b2ee928f98d3b93bad8b
David Chanko <david.chanko@gmail.com>
2017-03-19 16:44:52 -0400

    Calculation logic.

--- src/services/calculator.js
+++ src/services/calculator.js
@@ -1,6 +1,63 @@
 export function calculateScore(diamond, inclusion) {
-
+  let gradedInclusion = calculateInclusionScore(inclusion);
+  return scaleInclusionScore(diamond, gradedInclusion);
 };
+
+function calculateInclusionScore(inclusion) {
+  let score = log2(Math.sqrt(inclusion.length * 1000 * inclusion.width * 1000) / 50);
+  score = adjustForContrast(score, inclusion.contrast);
+  score = adjustForPosition(score, inclusion.position);
+  return  Object.assign({}, inclusion, {
+    grade: {
+      score: score,
+      gia: getGiaScore(score)
+    }
+  });
+}
+
+function log2(val) {
+  return Math.log(val) / Math.LN2;
+}
+
+function adjustForContrast(score, contrast) {
+  switch (contrast) {
+    case 1:
+      return score - 2.0;
+    case 2:
+      return score - 1.0;
+    case 3:
+      return score;
+    case 4:
+      return score + 0.5;
+    case 5:
+      return score + 1.0;
+    default:
+      return score;
+  }
+}
+
+function adjustForPosition(score, contrast) {
+  switch (contrast) {
+    case 1:
+      return score;
+    case 2:
+      return score < 5 ? score - 0.25 : score;
+    case 3:
+      if (score < 5) return score - 0.5;
+      else if (score < 6) return score - 0.25;
+      else return score;
+    case 4:
+      if (score < 5) return score - 1.0;
+      else if (score < 6) return score - 0.5;
+      else return score;
+    default:
+      return score;
+  }
+}
+
+function scaleInclusionScore(diamond, gradedInclusion) {
+  return gradedInclusion;
+}
 
 export function calculateCombinedScore(diamond, inclusions) {
   return Object.assign({}, diamond, combineGrade(inclusions));
@@ -26,8 +83,10 @@ function combineScores(inclusions) {
             : 0;
 }
 
+const LOG4 = Math.log(4);
+
 function log4(val) {
-  return Math.log(val) / Math.log(4);
+  return Math.log(val) / LOG4;
 }
 
 function invertScore(score) {
--- src/services/calculator.test.js
+++ src/services/calculator.test.js
@@ -5,19 +5,170 @@ import {
 
 
 describe('calculations', () => {
+  let diamond = { };
+  let result;
+  beforeEach(() => {
+    diamond = {
+      length: 6.5,
+      width: 6.5
+    };
+  });
 
-  describe('calculateCombinedScore', () => {
-    let diamond = { };
-    let inclusions = [];
-    let result;
+  describe('calculateScore', () => {
+    let inclusion = { };
+
     beforeEach(() => {
-      diamond = {
-        length: 6.5,
-        width: 6.5,
+      inclusion = {
+        length: 0.1,
+        width: 0.1,
         contrast: 3,
-        position: 1
+        position: 1,
+        grade: {
+          score: 0,
+          gia: ""
+        }
       };
     });
+
+    describe('no inclusion scaling', () => {
+
+      test('VVS1: no contrast adjustment', () => {
+        inclusion.length = 0.1;
+        inclusion.width = 0.1;
+        expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 1.0, gia: "VVS1" });
+      });
+
+       test('VVS2: no contrast adjustment', () => {
+        inclusion.length = 0.2;
+        inclusion.width = 0.2;
+        expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 2.0, gia: "VVS2" });
+      });
+
+      describe('contrast adjustment', () => {
+        beforeEach(() => {
+          inclusion.length = 0.4;
+          inclusion.width = 0.4;
+        });
+
+        test('light contrast (1): two grades better', () => {
+          inclusion.contrast = 1;
+          expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 1.0, gia: "VVS1" });
+        });
+
+        test('light-medium contrast (2): one grade better', () => {
+          inclusion.contrast = 2;
+          expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 2.0, gia: "VVS2" });
+        });
+
+        test('medium contrast (3): no change', () => {
+          inclusion.contrast = 3;
+          expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 3.0, gia: "VS1" });
+        });
+
+         test('medium-dark contrast (4): half-grade worse', () => {
+          inclusion.contrast = 4;
+          expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 3.5, gia: "VS1" });
+        });
+
+         test('dark contrast (5): one grade worse', () => {
+          inclusion.contrast = 5;
+          expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 4.0, gia: "VS2" });
+        });
+
+      });
+
+      describe('position adjustment', () => {
+
+        test('Under table (1): no adjustment', () => {
+          inclusion.position = 1;
+          expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 1.0, gia: "VVS1" });
+        });
+
+        describe('Just outside table (2)', () => {
+
+          beforeEach(() => {
+            inclusion.position = 2;
+          });
+
+          test('VS2 or better: quarter grade better', () => {
+            inclusion.length = 0.8;
+            inclusion.width = 0.8;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 3.75, gia: "VS1" });
+          });
+
+          test('SI1 or worse: no adjustment', () => {
+            inclusion.length = 1.6;
+            inclusion.width = 1.6;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 5.0, gia: "SI1" });
+          });
+
+        });
+
+        describe('Near girdle (3)', () => {
+
+          beforeEach(() => {
+            inclusion.position = 3;
+          });
+
+          test('VS2 or better: half grade better', () => {
+            inclusion.length = 0.8;
+            inclusion.width = 0.8;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 3.5, gia: "VS1" });
+          });
+
+          test('SI1 or better: quarter grade better', () => {
+            inclusion.length = 1.6;
+            inclusion.width = 1.6;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 4.75, gia: "VS2" });
+          });
+
+          test('SI2 or worse: no adjustment', () => {
+            inclusion.length = 3.2;
+            inclusion.width = 3.2;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 6.0, gia: "SI2" });
+          });
+
+        });
+
+        describe('Almost or touching girdle (4)', () => {
+
+          beforeEach(() => {
+            inclusion.position = 4;
+          });
+
+          test('VS2 or better: full grade better', () => {
+            inclusion.length = 0.8;
+            inclusion.width = 0.8;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 3.0, gia: "VS1" });
+          });
+
+          test('SI1 or better: half grade better', () => {
+            inclusion.length = 1.6;
+            inclusion.width = 1.6;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 4.5, gia: "VS2" });
+          });
+
+          test('SI2 or worse: no adjustment', () => {
+            inclusion.length = 3.2;
+            inclusion.width = 3.2;
+            expect(calculateScore(diamond, inclusion).grade).toEqual({ score: 6.0, gia: "SI2" });
+          });
+
+        });
+
+
+      });
+
+    });
+
+    describe('inclusion scaling', () => {
+
+    });
+
+  });
+
+  describe('calculateCombinedScore', () => {
+    let inclusions = [];
 
     test('returns copy of diamond', () => {
       result = calculateCombinedScore(diamond, inclusions);
